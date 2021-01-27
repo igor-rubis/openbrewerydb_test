4f2a789ed6d3b01d51a422bb2bd2e05dd838f3f8
Ihor <rubis.igor@gmail.com>
2021-01-27 02:11:55 +0200

    core set up

--- src/test/java/ExampleTestSuite.java
+++ src/test/java/ExampleTestSuite.java
@@ -1,21 +1,51 @@
+import io.qameta.allure.Allure;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
 import org.testng.Assert;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import static io.restassured.RestAssured.*;
-import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
 
 public class ExampleTestSuite {
+    PrintStream printToFile;
+    String fileLocation;
+    String fileName;
+
     @BeforeClass
     public void setup() {
         baseURI = System.getProperty("host");
         port = Integer.parseInt(System.getProperty("port"));
     }
 
+    @BeforeTest
+    public void preconditions() throws FileNotFoundException {
+        fileLocation = System.getProperty("user.dir") + "/build/tmp";
+        fileName = System.currentTimeMillis() + ".log";
+        File outputFile = new File(fileLocation, fileName);
+        FileOutputStream fileOutput = new FileOutputStream(outputFile);
+        printToFile = new PrintStream(fileOutput);
+    }
+
+    @AfterTest
+    public void attachments() throws IOException {
+        Path content = Paths.get(fileLocation + "\\" + fileName);
+        try (InputStream is = Files.newInputStream(content)) {
+            Allure.addAttachment("logs", is);
+        }
+    }
+
     @Test(description = "test 1")
     public void test1() {
-        given()
+        given().filters(new RequestLoggingFilter(printToFile), new ResponseLoggingFilter(printToFile))
                 .log().all()
                 .when().get("/breweries/search?query=dog")
                 .then()
